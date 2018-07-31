ad6e2b07665e2bddf6ee0581c231341683eefe3b
mandavaa <41896747+mandavaa@users.noreply.github.com>
2018-07-31 14:32:02 +0530

    commited main

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  Test Test!" );
+        System.out.println( "Hello World  Main Test!" );
     }
 
 	public int calculateSomething() {
